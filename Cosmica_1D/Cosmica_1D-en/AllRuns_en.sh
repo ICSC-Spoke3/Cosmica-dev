6c3a355b5b18432b7a73a53b82e43c3ba1495be9
Giovanni Cavallotto <giovanni.cavallotto@mib.infn.it>
2024-10-08 11:17:05 +0200

    Cleaning of the Cosmica 1D folder for easier usage

--- Cosmica_1D/Cosmica_1D-en/AllRuns_en.sh
+++ Cosmica_1D/Cosmica_1D-en/AllRuns_en.sh
@@ -1,4 +1,2 @@
 echo 'running... /home/nfsdisk/DevGC/Cosmica_1D/Cosmica_1D-en/runned_tests/AMS-02_PRL2015/run/run_AMS-02_PRL2015_Proton_TKO.run'
 bash /home/nfsdisk/DevGC/Cosmica_1D/Cosmica_1D-en/runned_tests/AMS-02_PRL2015/run/run_AMS-02_PRL2015_Proton_TKO.run 
-echo 'running... /home/nfsdisk/DevGC/Cosmica_1D/Cosmica_1D-en/runned_tests/AMS-02_PRL2014/run/run_AMS-02_PRL2014_Positron_TKO.run'
-bash /home/nfsdisk/DevGC/Cosmica_1D/Cosmica_1D-en/runned_tests/AMS-02_PRL2014/run/run_AMS-02_PRL2014_Positron_TKO.run 
--- Cosmica_1D/Trivial_1D-rigi/AllRuns_rigi.sh
+++ Cosmica_1D/Trivial_1D-rigi/AllRuns_rigi.sh
@@ -1,4 +1,2 @@
 echo 'running... /home/nfsdisk/DevGC/Cosmica_1D/Trivial_1D-rigi/runned_tests/AMS-02_PRL2015/run/run_AMS-02_PRL2015_Proton_TKO.run'
 bash /home/nfsdisk/DevGC/Cosmica_1D/Trivial_1D-rigi/runned_tests/AMS-02_PRL2015/run/run_AMS-02_PRL2015_Proton_TKO.run 
-echo 'running... /home/nfsdisk/DevGC/Cosmica_1D/Trivial_1D-rigi/runned_tests/AMS-02_PRL2014/run/run_AMS-02_PRL2014_Positron_TKO.run'
-bash /home/nfsdisk/DevGC/Cosmica_1D/Trivial_1D-rigi/runned_tests/AMS-02_PRL2014/run/run_AMS-02_PRL2014_Positron_TKO.run 
