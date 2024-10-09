d0970f6894d2d1d7380efe4a5175c0ae517805e2
giovannicavallotto <giovanni.cavallotto@unimib.it>
2024-10-09 12:52:36 +0200

    Cleaning the unstable versions

--- Cosmica_V5-speedtest/headers/HeliosphericPropagation.cuh
+++
@@ -1,7 +0,0 @@
-#ifndef HeliosphericPropagation
-#define HeliosphericPropagation
-
-// Propagation kernel cycle collecting the stocastic propagation computations of quasi-particle
-__global__ void HeliosphericProp(int, float, float, float, struct QuasiParticle_t, int*, struct PartDescription_t, curandStatePhilox4_32_10_t*, float*);
-
-#endif
