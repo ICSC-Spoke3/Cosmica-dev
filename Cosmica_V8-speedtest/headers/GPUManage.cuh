e2ba407769a80f6d39c68b631caa78e6960887a3
Matteo Grazioso <884055@stud.unive.it>
2025-03-03 11:30:49 +0100

    Adding docs in GPUManage

--- Cosmica_V8-speedtest/headers/GPUManage.cuh
+++ Cosmica_V8-speedtest/headers/GPUManage.cuh
@@ -3,28 +3,15 @@
 
 void HandleError(cudaError_t, const char *, int);
 
-/* Manage error in GPU
-   */
-struct LaunchParam_t RoundNpart(unsigned, cudaDeviceProp, bool, int);
+constexpr unsigned long hash(const std::string_view &);
 
-/* Round the number of particle to be simulated based on the GPU capability, returning a struct with threads
-   and blocks count together with shared memory bytes.
-   The calculations are done based on the cuda occupancy calculator output to maximize the usage of the GPUs.
-   */
+consteval unsigned long operator""_(const char *, const size_t);
 
 int BestNWarpPerBlock(char, bool);
 
-/* Define the best value of NWarpPerBlock for a given GPU (name) at the website
-   https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#compute-capabilities in table 15 one may
-   found the value of Maximum number of resident warps per SM (MSWSM) for each Compute Capability. The number
-   of WarpPerBlock should be a divisor of MSWSM to allow a full occupancy of SM. The best value of WarpPerBlock
-   should be found with a performance study, together with Cuda Occupancy Calculator.
-   */
+LaunchParam_t RoundNpart(unsigned, cudaDeviceProp, bool, int);
 
 cudaDeviceProp *DeviceInfo(int, bool);
 
-/* Retrive the GPU properties (info) for the selected GPU and print a summary of useful infos for debugging,
-   with the verbose option.
-   */
 
 #endif
--- Cosmica_V8-speedtest/sources/GPUManage.cu
+++ Cosmica_V8-speedtest/sources/GPUManage.cu
@@ -7,7 +7,12 @@
 #include "VariableStructure.cuh"
 #include "GenComputation.cuh"
 
-
+/**
+ * @brief HandleError
+ * @param err cudaError_t error
+ * @param file the file where the error occurred
+ * @param line the line where the error occurred
+ */
 void HandleError(const cudaError_t err, const char *file, const int line) {
     if (err != cudaSuccess) {
         printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
@@ -15,16 +20,33 @@ void HandleError(const cudaError_t err, const char *file, const int line) {
     }
 }
 
+/**
+ * @brief hash function to convert a string into a hash
+ * @param str the string to be hashed
+ * @return the hash of the string
+ */
 constexpr unsigned long hash(const std::string_view &str) {
     unsigned long hash = 0;
     for (const auto &e: str) hash = hash * 131 + e;
     return hash;
 }
 
+/**
+ * @brief operator""_ to convert a string into a hash
+ * @param str the string to be hashed
+ * @param len the length of the string
+ * @return the hash of the string
+ */
 consteval unsigned long operator""_(const char *str, const size_t len) {
     return hash(std::string_view(str, len));
 }
 
+/**
+ * @brief Define the best value of NWarpPerBlock for a given GPU (name)
+ * @param name the name of the GPU
+ * @param verbose the verbosity of the output
+ * @return the best warp per block
+ */
 int BestWarpPerBlock(char name[], const int verbose) {
     int BestWarpPerBlock = 8;
 
@@ -48,11 +70,20 @@ int BestWarpPerBlock(char name[], const int verbose) {
     return BestWarpPerBlock;
 }
 
+/**
+ * @brief Round the number of particle to be simulated based on the GPU capability, returning a struct with threads
+ * and blocks count together with shared memory bytes.
+ * The calculations are done based on the cuda occupancy calculator output to maximize the usage of the GPUs.
+ * @param NPart the number of particles
+ * @param GPUprop the GPU properties
+ * @param verbose the verbosity of the output
+ * @param WpB the warp per block
+ * @return the launch parameters
+ */
 LaunchParam_t RoundNpart(const unsigned NPart, cudaDeviceProp GPUprop, const bool verbose, const int WpB) {
     LaunchParam_t launch_param;
 
     // Computation of the number of blocks, warp per blocks, threads per block and shared memory bits
-    // launch_param.Npart = ceil_int(NPart, GPUprop.warpSize) * GPUprop.warpSize;
     int WarpPerBlock = WpB <= 0 ? BestWarpPerBlock(GPUprop.name, verbose) : WpB;
     launch_param.threads = WarpPerBlock * GPUprop.warpSize;
     launch_param.blocks = ceil_int_div(NPart, launch_param.threads);
@@ -97,15 +128,19 @@ LaunchParam_t RoundNpart(const unsigned NPart, cudaDeviceProp GPUprop, const boo
     return launch_param;
 }
 
+/**
+ * @brief Retrieve the GPU properties (info) for the selected GPU and print a summary of useful infos for debugging,
+ * with the verbose option.
+ * @param N_GPU_count the number of GPU
+ * @param verbose the verbosity of the output
+ * @return the GPU properties
+ */
 cudaDeviceProp *DeviceInfo(const int N_GPU_count, const bool verbose) {
     const auto infos = new cudaDeviceProp[N_GPU_count];
 
-    // Retrive the GPU properties (info) for each available GPU
     for (int i = 0; i < N_GPU_count; i++)
         cudaGetDeviceProperties(&infos[i], i);
 
-
-    // Print all the GPU info useful for debugging
     if (verbose) {
         printf("----- GPU infos -----\n");
         printf("There are %d CUDA enabled devices \n", N_GPU_count);
