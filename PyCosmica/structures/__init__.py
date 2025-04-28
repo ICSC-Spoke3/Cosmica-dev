5dd84b2c7e077b1b8b372b005974e99217d208f2
Leone Bacciu <leonebacciu@gmail.com>
2025-04-28 09:40:24 +0200

    Remove old folders for publication

--- PyCosmica/structures/__init__.py
+++
@@ -1,6 +0,0 @@
-from .constants import *
-from .io import Options, HeliosphericParameters, HeliosheatParameters
-from .propagation import MonteCarloResult, SimulatedHeliosphere, SimParameters, SimParametersJit, QuasiParticle, \
-    PropagationState, PropagationConstantsItem, PropagationConstants
-from .sde import ConvectionDiffusionTensor, DiffusionTensor
-from .shared import *
--- extra/Simulations_K0Search/HM4C_Code/HelMod-4-CUDA_v3_1/readme.md
+++
@@ -1,2 +0,0 @@
-# HelMod-4 Code - CUDA Versione
-Versione scritta in C-CUDA del modello HelMod-4
