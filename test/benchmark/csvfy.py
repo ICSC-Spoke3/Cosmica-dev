9a6c10fc0c8ccdd81b424a4ca33e3d3ea0c35494
Leone Bacciu <leonebacciu@gmail.com>
2025-06-23 10:55:20 +0200

    Update csvfy

--- test/benchmark/csvfy.py
+++ test/benchmark/csvfy.py
@@ -8,18 +8,19 @@ if __name__ == '__main__':
     SET = ''
 
     data_dir = Path(__file__).parent.parent / 'data'
-    p_outputs = data_dir / 'benchmark' / f'outputs{SET}'
+    p_outputs = data_dir / 'benchmark' / f'results'
 
     data = []
-    for exetime_path in p_outputs.glob('*/exetime_*.txt'):
+    for exetime_path in p_outputs.rglob('exetime_*.csv'):
         with open(exetime_path, 'r') as f:
-            exetime = float(f.read())
+            exetime = sum(map(float, f.readlines()))
+        device = exetime_path.parent.parent.name.split('_')[-1]
         spec = exetime_path.parent.name.split('_')
         version = exetime_path.stem.split('_')[-1]
         date, test, val = spec[0], spec[2], spec[3]
-        data.append((date, test, val, version, exetime))
+        data.append((date, test, val, version, device, exetime))
 
-    df = pd.DataFrame(data, columns=['date', 'test', 'val', 'version', 'exetime'])
+    df = pd.DataFrame(data, columns=['date', 'test', 'val', 'version', 'device', 'exetime'])
     print(df)
-    df.to_csv(f'benchmark{SET}.csv', index=False)
+    df.to_csv(f'benchmark.csv', index=False)
 
